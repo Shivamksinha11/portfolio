5b918abec17b01b8dcd5309457e4cbaaddcec12b
Shivamksinha11 <67308104+Shivamksinha11@users.noreply.github.com>
2021-06-10 23:36:15 +0530

    Update Head.js

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,6 +1,5 @@
 import './Head.css';
 import {Link} from 'react-scroll';
-import Typist from 'react-typist';
 
 const Head = () => {
     return (
@@ -8,12 +7,12 @@ const Head = () => {
             
         <div className="header">
             <h1 className="heading-primary">
-                <Typist>
-                <Typist.Delay ms={500} />
+                
+               
                     <span className="subheader">Hello, I'm <span class="name">Shivam </span>.</span> 
                     <span className="subheader2">I'm a full stack Web developer.</span>   
                     <Link to="abc" smooth={true} duration={2000}><button className="neo btn1">View My Work <span class="arrow">&#8594;</span></button></Link>
-                </Typist> 
+                
             </h1>
             
         </div>
